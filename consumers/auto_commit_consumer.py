2fd05d9a5e04f597c1ffe8827b13f2407a1e0401
휘래 김 <hramst0618@gmail.com>
2025-09-20 17:08:51 +0900

    feat: enable.auto.commit Consumer 작성

--- consumers/auto_commit_consumer.py
+++ consumers/auto_commit_consumer.py
@@ -1,8 +1,8 @@
-import json
+from confluent_kafka import Consumer
+from confluent_kafka import KafkaException
+from consumers.base_consumer import BaseConsumer
 import pandas as pd
-from confluent_kafka import Consumer, KafkaException
-
-from base_consumer import BaseConsumer
+import json
 
 
 class AutoCommitConsumer(BaseConsumer):
@@ -12,10 +12,11 @@ class AutoCommitConsumer(BaseConsumer):
 
         conf = {'bootstrap.servers': self.BOOTSTRAP_SERVERS,
                 'group.id': self.group_id,
-                'auto.offset.reset': 'lastest',
+                'auto.offset.reset': 'latest',
                 'enable.auto.commit': 'true',
-                'auto.commit.interval.ms': 6000  # default : 5000(5초)
+                'auto.commit.interval.ms': '60000'         # 기본 값: 5000 (5초)
                 }
+
         self.consumer = Consumer(conf)
         self.consumer.subscribe(self.topics, on_assign=self.callback_on_assign)
 
@@ -23,11 +24,11 @@ class AutoCommitConsumer(BaseConsumer):
     def poll(self):
         try:
             while True:
-                msg_list = self.consumer.consume(num_messages=100)
-                if msg_list is None or len(msg_list) == 0: continue
+                msg_lst = self.consumer.consume(num_messages=100)
+                if msg_lst is None or len(msg_lst) == 0: continue
 
-                self.logger.info(f'message count:{len(msg_list)}')
-                for msg in msg_list:
+                self.logger.info(f'message count:{len(msg_lst)}')
+                for msg in msg_lst:
                     error = msg.error()
                     if error:
                         self.handle_error(msg, error)
@@ -35,18 +36,21 @@ class AutoCommitConsumer(BaseConsumer):
                 # 로직 처리 부분
                 # Kafka 레코드에 대한 전처리, Target Sink 등 수행
                 self.logger.info(f'message 처리 로직 시작')
-                msg_val_list = [json.loads(msg.value().decode('utf-8')) for msg in msg_list]
-                df = pd.DataFrame(msg_val_list)
+                msg_val_lst = [json.loads(msg.value().decode('utf-8')) for msg in msg_lst]
+                df = pd.DataFrame(msg_val_lst)
                 print(df[:10])
+
 
         except KafkaException as e:
             self.logger.exception("Kafka exception occurred during message consumption")
+
         except KeyboardInterrupt:  # Ctrl + C 눌러 종료시
             self.logger.info("Shutting down consumer due to keyboard interrupt.")
 
         finally:
             self.consumer.close()
             self.logger.info("Consumer closed.")
+
 
 if __name__ == '__main__':
     auto_commit_consumer = AutoCommitConsumer('auto_commit_consumer')
